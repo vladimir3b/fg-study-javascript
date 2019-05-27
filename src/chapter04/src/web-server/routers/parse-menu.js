4b5d96c7cb8d02a550e96b2749cdb6de0b0b61e8
VladimirIoan <vladimir_3b@hotmail.com>
2019-05-27 11:42:58 +0300

    menu is working

--- src/chapter04/src/web-server/routers/parse-menu.js
+++ src/chapter04/src/web-server/routers/parse-menu.js
@@ -6,17 +6,20 @@ function link(page_code) {
 
 module.exports = function () {
   return siteStructure.menu.map(item => {
-
+    // console.log(item.pages);
     return {
       title: item.title,
       icon: item.icon,
-      pages: item.pages.map(page_code => {
+      pages: (item.pages.length !== 1) ? item.pages.map(page_code => {
         const searchedPage = link(page_code);
         return {
           title: searchedPage.title,
           url: searchedPage.route
         };
-      }),
+      }) : {
+        title: link(item.pages[0]).title,
+        url: link(item.pages[0]).route
+      },
       unique: item.pages.length === 1
    }
   });
